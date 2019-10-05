4e060892f235720c0809ff285ebe8307ace5f3ff
Ayoola John <ayo.johnsu@gmail.com>
2019-10-04 23:49:22 -0500

    working app

--- GalleryARt/ViewController.swift
+++ GalleryARt/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 import SpriteKit
 import ARKit
 
+
+struct ImageInformation {
+    let name: String
+    let description: String
+    let image: UIImage
+}
+
 class ViewController: UIViewController, ARSKViewDelegate {
     
     @IBOutlet var sceneView: ARSKView!
+    
+    var selectedImage : ImageInformation?
+    
+    let images = ["inevitable" : ImageInformation(name: "Inevitable Human", description: "The Mona Lisa is a half-length portrait painting by the Italian Renaissance artist Leonardo da Vinci that has been described as 'the best known, the most visited, the most written about, the most sung about, the most parodied work of art in the world'.", image: UIImage(named: "inevitable")!)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,12 +39,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
         if let scene = SKScene(fileNamed: "Scene") {
             sceneView.presentScene(scene)
         }
-    }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        
         // Get a reference to the images in the AR resource group
         guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "photogallery" , bundle: nil) else {
             fatalError("Missing expected asset catalog resources.")
@@ -47,6 +53,12 @@ class ViewController: UIViewController, ARSKViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -59,12 +71,42 @@ class ViewController: UIViewController, ARSKViewDelegate {
     // MARK: - ARSKViewDelegate
     
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
-        // Create and configure a node for the anchor added to the view's session.
-        let labelNode = SKLabelNode(text: "👾")
+        
+        if let imageAnchor = anchor as? ARImageAnchor,
+            let referenceImageName = imageAnchor.referenceImage.name,
+            let scannedImage = self.images[referenceImageName] {
+            
+            self.selectedImage = scannedImage
+            
+            self.performSegue(withIdentifier: "ShowImageInformation", sender: self)
+            
+            return imageSeenMarker()
+        }
+        
+        return nil
+
+    }
+    
+    
+    
+    // Create and configure a node for the anchor added to the view's session.
+    private func imageSeenMarker() -> SKLabelNode {
+        let labelNode = SKLabelNode(text: "✅")
         labelNode.horizontalAlignmentMode = .center
         labelNode.verticalAlignmentMode = .center
         return labelNode;
     }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showImageInformation"{
+            if let imageInformationVC = segue.destination as? ImageInformationViewController,
+                let actualSelectedImage = selectedImage {
+                imageInformationVC.imageInformation = actualSelectedImage
+            }
+        }
+    }
+    
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
